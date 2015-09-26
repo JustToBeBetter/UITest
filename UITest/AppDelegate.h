6988236c4b8b03e174d7606c0fd69a303a7d500c
李金柱 <li@li.local>
2015-09-26 20:34:53 +0800

    Initial Commit

---
+++ UITest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITest
+//
+//  Created by 李金柱 on 15/9/26.
+//  Copyright (c) 2015年 李金柱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITest
+//
+//  Created by 李金柱 on 15/9/26.
+//  Copyright (c) 2015年 李金柱. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
